926feb05a822b58398ace9cb9b5bd9a897f3b1b0
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-28 10:01:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ RED='\033[91m'
 
 echo -e "$BLUE[*]$RESET Installing buggy... $RESET"
 sudo apt update
-sudo apt install -y python3 python3-requests python3-pip python3-lxml python3-requests openssl ca-certificates python3-dev wget git
+sudo apt-get install -y python3 python3-requests python3-pip python3-lxml python3-requests openssl ca-certificates python3-dev wget git
 cp -f $PWD/buggy.py /usr/bin/buggy
 cp -f $PWD/buggy2.py /usr/bin/buggy2.py
 cp -f $PWD/buggy.desktop /usr/share/applications/
